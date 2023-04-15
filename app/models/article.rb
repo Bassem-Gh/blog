3ffa7ff0ecdf95b18d35907f5c4e8e9c75c328dc
Bassem GH <bessem.gharsellaoui@gmail.com>
2023-04-15 18:29:05 +0100

    using concerns

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,10 @@
 
 # articles model
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
